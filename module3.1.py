d2868a186b55a6939b6b57527b8bb39b649e2832
aungthuyahan <aungthu.ya659@gmail.com>
2025-08-25 17:26:18 +0630

    Initial commit for 3.1

---
+++ module3.1.py
@@ -0,0 +1,6 @@
+fisher_zander = float(input("Enter length of zander in cm: "))
+if fisher_zander < 42:
+    print("Release the zander as it does not fulfill the size limit.\nIt should be at least 42cm long.")
+else:
+    print("You are lucky!")
+
